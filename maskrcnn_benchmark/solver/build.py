c3d7d5c4fff373e4b26fc6e9e3bd33ddd50b01ff
zhangliliang <zhangll.level0@gmail.com>
2019-05-15 08:53:51 +0800

    [NEW] set the bias lr and decay in syncbn.

--- maskrcnn_benchmark/solver/build.py
+++ maskrcnn_benchmark/solver/build.py
@@ -1,19 +1,35 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 import torch
+import apex
 
 from .lr_scheduler import WarmupMultiStepLR
 
 
 def make_optimizer(cfg, model):
+    syncbn_layer_names = set()
+    for key, value in model.named_modules():
+        if isinstance(value, apex.parallel.SyncBatchNorm):
+            syncbn_layer_names.add(key)
+
     params = []
     for key, value in model.named_parameters():
         if not value.requires_grad:
             continue
         lr = cfg.SOLVER.BASE_LR
         weight_decay = cfg.SOLVER.WEIGHT_DECAY
+        bias_in_syncbn = False
+
         if "bias" in key:
-            lr = cfg.SOLVER.BASE_LR * cfg.SOLVER.BIAS_LR_FACTOR
-            weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
+            for syncbn_layer_name in syncbn_layer_names:
+                if syncbn_layer_name in key:
+                    bias_in_syncbn = True
+                    break
+            if bias_in_syncbn:
+                lr = cfg.SOLVER.BASE_LR * cfg.SOLVER.SYNCBN_BIAS_LR_FACTOR
+                weight_decay = cfg.SOLVER.SYNCBN_WEIGHT_DECAY_BIAS
+            else:
+                lr = cfg.SOLVER.BASE_LR * cfg.SOLVER.BIAS_LR_FACTOR
+                weight_decay = cfg.SOLVER.WEIGHT_DECAY_BIAS
         params += [{"params": [value], "lr": lr, "weight_decay": weight_decay}]
 
     optimizer = torch.optim.SGD(params, lr, momentum=cfg.SOLVER.MOMENTUM)
